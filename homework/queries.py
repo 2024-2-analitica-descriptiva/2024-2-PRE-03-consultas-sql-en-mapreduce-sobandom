5d5549ca4ecbc4a49d5a6b34428d7fc0941c0aa4
sobandom <sobandom@unal.edu.co>
2024-11-15 16:30:26 -0500

    taller 3 - descriptiva

--- homework/queries.py
+++ homework/queries.py
@@ -14,9 +14,20 @@ from .mapreduce import run_mapreduce_job # type: ignore
 # SELECT *, tip/total_bill as tip_rate
 # FROM tips;
 #
-def mapper_query_1(sequence):
-    print(sequence)
 
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip() + ",tip_rate"))
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
 
 def reducer_query_1(sequence):
     """Reducer"""
@@ -25,6 +36,7 @@ def reducer_query_1(sequence):
 
 #
 # ORQUESTADOR:
+#
 #
 def run():
     """Orquestador"""
@@ -38,3 +50,5 @@ def run():
 
 
 if __name__ == "__main__":
+
+    run()
