38ef6ea0494ebe48ee44bf2952fd4969508a7ace
sobandom <sobandom@unal.edu.co>
2024-11-15 15:41:29 -0500

    taller 3- descriptiva

--- homework/queries.py
+++ homework/queries.py
@@ -3,6 +3,25 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
+from .mapreduce import run_mapreduce_job # type: ignore
+
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+#
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+def mapper_query_1(sequence):
+    print(sequence)
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
+
 
 #
 # ORQUESTADOR:
@@ -10,7 +29,12 @@
 def run():
     """Orquestador"""
 
+    run_mapreduce_job(
+        mapper=mapper_query_1,
+        reducer=reducer_query_1,
+        input_directory="files/input",
+        output_directory="files/query_1",
+    )
+
 
 if __name__ == "__main__":
-
-    run()
